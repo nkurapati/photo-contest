c1807e55274c5e4d3a847c3ab625f822ffc5f0a7
Venkat <venkat@divami.com>
2016-07-05 15:48:55 +0530

    Added the functionality for uploading the photos to the server.

--- app/js/controllers/upload.js
+++ app/js/controllers/upload.js
@@ -7,8 +7,9 @@ function uploadController($scope, restService, popupService, authService) {
 	}
 	$scope.onFileChange;
 	$scope.imgUrl;
+	$scope.prevId = null;
 	
-	$scope.photos = [{}];
+	$scope.photos = [];
 	
 	$scope.addAnotherFile = function() {
 		$scope.photos.push({});
@@ -18,18 +19,92 @@ function uploadController($scope, restService, popupService, authService) {
 		$scope.photos.splice(index, 1);
 	}
 
+	$scope.selectPhotosFromLocal = function() {
+		var elem = document.getElementById('file_upload_field');
+		elem.click();
+	}
+
 	$scope.onInputChange = function(elem) {
+		var filesLen = elem.files.length;
 		$scope.$apply(function(scope) {
-			var index = elem.getAttribute('data-index');
+			for (var i = 0; i < filesLen; i++) {
+				var obj = {};
+				var file = elem.files[i];
+
+				obj.file = file;
+				obj.uniqueId = file.name + "_" + Math.random();
+				obj.imgUrl = window.URL.createObjectURL(file);
+				obj.title = "";
+				obj.description = "";
+				obj.showTitleOverlay = false;
+				$scope.photos.push(obj);
+			}
+		});
+		
+
+		/**/
+
+		//$scope.$apply(function(scope) {
+			
+			/*var index = elem.getAttribute('data-index');
 	        var photofile = elem.files[0];
 			$scope.photos[index].file = photofile;
-	        $scope.imgUrl = window.URL.createObjectURL(photofile);
+	        $scope.imgUrl = window.URL.createObjectURL(photofile);*/
 			/*var reader = new FileReader();
 			reader.onload = function(e) {
 			// handle onload
 			};
 			reader.readAsDataURL(photofile);*/
-	     });
+	    // });
+	}
+
+	$scope.addDetailsAboutPhoto = function() {
+		alert("Add photo details");
+	}
+
+	/**
+	 * shows the overlay for entering the details about the photo of the current selected photo
+	*/ 
+	$scope.showTitleOverlay = function(id) {
+		$scope.tooglePreviousElementOverlay();
+		$scope.photos[id].showTitleOverlay = true;
+		$scope.prevId = id;
+	}
+
+	/**
+	 * this function hides the overlay of the element based on the index.
+	*/ 
+	$scope.hideOverlay = function(index) {
+		$scope.photos[index].showTitleOverlay = false;
+	}
+
+	/**
+	 * this function checks if there is any overlay opened otherthan the current selected elememt and toogles it state.
+	*/ 
+	$scope.tooglePreviousElementOverlay = function() {
+		if ($scope.prevId != null && $scope.photos[$scope.prevId].showTitleOverlay) {
+			$scope.photos[$scope.prevId].showTitleOverlay = false;
+		}
+	}
+
+	/**
+	 * this function uploads the photos to the server
+	*/ 
+	$scope.uploadPhotosToServer = function() {
+		for (var i = 0, len = $scope.photos.length; i < len; i++) {
+			var formData = new FormData();
+			var photo = $scope.photos[i];
+
+			formData.append('email', userData.email);
+			formData.append('name', userData.name);
+			formData.append('title', photo.title);
+			formData.append('description', photo.description);
+			formData.append('file', photo.file);
+			formData.append('uniqueId', photo.uniqueId);
+			//formData.append('uniqueId', p);
+
+			restService.upload('upload', formData).then(onSuccess);
+		}
 	}
 	
 	$scope.onSubmission = function() {
